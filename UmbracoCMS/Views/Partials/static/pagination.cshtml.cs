2b3fdf8ac4a5b4c7b0db9868596fae86ae63576a
Rickard <rickard_b@live.com>
2024-09-24 19:23:40 +0200

    pagination, about page, recent projects,projects page

---
+++ UmbracoCMS/Views/Partials/static/pagination.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UmbracoCMS.Views.Partials.static
+{
+    public class paginationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
